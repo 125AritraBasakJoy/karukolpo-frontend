ac306749de185d2a78069cb90c24299b6cd64768
125AritraBasakJoy <basakjoy125@gmail.com>
2026-02-22 23:15:19 +0600

    Plugin update.

---
+++ src/types/netlify-runtime.d.ts
@@ -0,0 +1,7 @@
+declare module '@netlify/angular-runtime/context' {
+    export function getContext(): any;
+}
+
+declare module '@netlify/angular-runtime/context.mjs' {
+    export function getContext(): any;
+}
